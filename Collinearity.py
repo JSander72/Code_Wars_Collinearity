47c9402920c740cb945db28383806b5184b9e2c0
JSander72 <jamesearlsanders2@outlook.com>
2024-07-27 19:49:02 -0400

    corrected import of numpy but didnt need it

--- Collinearity.py
+++ Collinearity.py
@@ -1,10 +1,3 @@
-import numpy as np
-
 def collinearity(x1, y1, x2, y2):
-    vec1 = (x2 - x1)
-    vec2 = (y2 - y1)
-
-
-np.array([x1, y1, x2, y2])
-
-
+    cross_product = x1 * y2 - y1 * x2
+    return cross_product == 0
